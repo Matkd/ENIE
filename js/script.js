44a56cf8b5c9cf7cfb6cdb51c4727023ae5905f3
MatiD <dildamatias@gmail.com>
2022-06-03 20:15:36 -0300

    Fin

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-alert ("si funciona")
+let cont = 0;
+let suma = 0;
+let historial = document.querySelector("#historial");
+let promedio = document.querySelector("#promedio");
+
+    function agregar(){
+        let nombre = document.querySelector("#nombre").value;
+        let gasto = document.querySelector("#gasto").value;
+
+        historial.innerHTML = historial.innerHTML + nombre+": $"+gasto+"<br>";
+
+        suma = suma + parseInt(gasto);
+
+        cont = cont + 1;
+
+        promedio.innerHTML = "Gasto total: $ "+suma+"<br>Cada uno debe poner: $"+(Math.round(suma / cont));
+    }
+
+document.querySelector("button").addEventListener("click",function(evento){
+    evento.preventDefault(); evento.stopPropagation();
+    agregar();
+});
